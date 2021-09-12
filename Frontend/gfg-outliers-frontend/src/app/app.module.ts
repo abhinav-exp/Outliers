45ec62ac09abacae3e1dda9567607e17f3f86e21
Umang <umangagarwal345@gmail.com>
2021-09-12 11:33:27 +0530

    Added Timetable section

--- Frontend/gfg-outliers-frontend/src/app/app.module.ts
+++ Frontend/gfg-outliers-frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { TimetableComponent } from './components/content-page/timetable/timetable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    MainContentComponent,
+    TimetableComponent
   ],
   imports: [
     BrowserModule,
