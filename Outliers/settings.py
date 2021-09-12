879610bfe266350116aef4fe15d6076d50c39618
abhinav-exp <kabhinav482@gmail.com>
2021-09-12 13:13:12 +0530

    collectstatic

--- Outliers/settings.py
+++ Outliers/settings.py
@@ -124,6 +124,7 @@ MEDIA_URL = '/'
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Frontend/gfg-outliers-frontend/dist_gfg/gfg-outliers-frontend')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
