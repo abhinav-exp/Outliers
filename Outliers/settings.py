94b84e76df75718f6d6f8f542eb668a66fb36290
abhinav-exp <kabhinav482@gmail.com>
2021-09-13 18:02:36 +0530

    :)

--- Outliers/settings.py
+++ Outliers/settings.py
@@ -37,11 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
-    'corsheaders',
-=======
     'rest_framework',
->>>>>>> master
+    'corsheaders',
     'Hostingapp',
     'API',
 ]
