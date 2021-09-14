8358f6fbbdfad8d513f3ba3edede496d5871abfb
Umang <umangagarwal345@gmail.com>
2021-09-14 22:02:21 +0530

    Added login/signup page

--- Frontend/gfg-outliers-frontend/src/app/app.component.ts
+++ Frontend/gfg-outliers-frontend/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gfg-outliers-frontend';
+  isAuthenticated:boolean = false;
+  loginMode=true;
+  signUpMode=false;
+
+  ngOnInit(): void {
+    this.isAuthenticated = false;
+    this.signUpMode = false;
+    this.loginMode = true;
+  }
+
+  switchToSignUp(event:any)
+  {
+      this.signUpMode = true;
+      this.loginMode = false;
+  }
+  switchToLogin(event:any)
+  {
+      this.signUpMode = false;
+      this.loginMode = true;
+  }
+
+  authenticateForNow(event:any)
+  {
+      this.isAuthenticated = true;
+  }
+  
+
+
 }
