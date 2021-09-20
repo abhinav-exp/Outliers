28ebe1f16a9e38d3a3be7ab65d4464bfff4b4b82
Umang <umangagarwal345@gmail.com>
2021-09-20 20:06:56 +0530

    Added class representative exclusive functionalities in notice board

--- Frontend/gfg-outliers-frontend/src/app/components/content-page/notice-board/notice-board.component.ts
+++ Frontend/gfg-outliers-frontend/src/app/components/content-page/notice-board/notice-board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/authentication-service.service';
 import { ContentService } from 'src/app/content-service.service';
 
 @Component({
@@ -12,15 +13,30 @@ export class NoticeBoardComponent implements OnInit {
   noticePicked:any;
 
   listOfNotice:any;
+  _isCR_authenticated:boolean = false;
+  _isAddEditMode:boolean = false;
+  _addEditTitle:string = 'Edit Notice';
+  _activeNoticeForAddEdit:any;
+  _activeState:number = 0;
+  _noticeModel:any = null;
   
-  constructor(private contentService:ContentService) 
+  constructor(private contentService:ContentService
+            ,private _authService:AuthenticationService) 
   { 
        
   }
 
   ngOnInit(): void 
   {
-    this.contentService.getNoticeBoardList().subscribe((resp)=>{
+    this._noticeModel = null;
+    this._isCR_authenticated = this._authService._isCR_Aunthenticated;
+    this._isAddEditMode = false;
+    this.loadNotice();
+  }
+
+  loadNotice()
+  {
+      this.contentService.getNoticeBoardList().subscribe((resp)=>{
         console.log(resp);
         this.listOfNotice = resp;
         this.noticePicked = []; 
@@ -34,5 +50,83 @@ export class NoticeBoardComponent implements OnInit {
      this.noticePicked = []; 
      this.noticePicked.push(event);
   }
+
+  openEditNoticePage(notice:any)
+  {
+      this._isAddEditMode = true;
+      this._addEditTitle = 'Edit Notice'
+      this._activeNoticeForAddEdit = notice;
+      this._noticeModel = notice.text;
+      this._activeState = 1;
+  }
+
+  openAddNoticePage()
+  {
+      this._isAddEditMode = true;
+      this._addEditTitle = 'Add Notice'
+      this._noticeModel = '';
+      this._activeState = 2;
+  }
+
+  saveAndPost()
+  {
+       if(this._activeState==1)
+       {
+          this.editNotice();
+       }
+       else{
+          this.createNotice();
+       }
+  }
+
+  backToListPage()
+  {
+    this.loadNotice();
+    this._isAddEditMode = false;
+  }
+
+  editNotice()
+  {
+      if(this._noticeModel==null || this._noticeModel==undefined || this._noticeModel.length==0 )
+      {
+          return;
+      }
+
+      let _body = {
+        notice_id : this._activeNoticeForAddEdit.id,
+        text : this._noticeModel
+      }
+
+      this.contentService.editNotice(_body).subscribe((resp)=>{
+          this.loadNotice();
+          this._isAddEditMode = false;
+      })
+  }
+
+  createNotice()
+  {
+      if(this._noticeModel==null || this._noticeModel==undefined || this._noticeModel.length==0 )
+      {
+          return;
+      }
+
+      let _body = {
+        student_id : this._authService.activeId,
+        text : this._noticeModel
+      }
+
+      this.contentService.createNotice(_body).subscribe((resp)=>{
+          this.loadNotice();
+          this._isAddEditMode = false;
+      })
+  }
+
+  deleteNotice(noticeId:any)
+  {
+       this.contentService.deleteNotice(noticeId).subscribe((resp)=>{
+           this.loadNotice();
+       })
+  }
+
 
 }
